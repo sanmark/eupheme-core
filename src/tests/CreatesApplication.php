7e3402ae9e995628b0a68c7120f79369c1b67360
Supun Budhajeewa <budhajeewa@thesanmark.com>
2018-06-20 11:50:44 +0530

    Reformat code.

--- src/tests/CreatesApplication.php
+++ src/tests/CreatesApplication.php
@@ -1,22 +1,24 @@
 <?php
 
-namespace Tests;
+namespace Tests ;
 
-use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Contracts\Console\Kernel ;
 
 trait CreatesApplication
 {
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
 
-        $app->make(Kernel::class)->bootstrap();
+	/**
+	 * Creates the application.
+	 *
+	 * @return \Illuminate\Foundation\Application
+	 */
+	public function createApplication ()
+	{
+		$app = require __DIR__ . '/../bootstrap/app.php' ;
 
-        return $app;
-    }
+		$app -> make ( Kernel::class ) -> bootstrap () ;
+
+		return $app ;
+	}
+
 }
