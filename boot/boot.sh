2dbfde8c318c710bbe584368cbb80a9cb9a1601a
Supun Budhajeewa <budhajeewa@thesanmark.com>
2018-06-20 12:05:45 +0530

    Assign the ownership of `app/storage` dir to `www-data`.

--- boot/boot.sh
+++ boot/boot.sh
@@ -7,5 +7,5 @@ for i in "${vars[@]}"
 do
 	echo "fastcgi_param $i \"${!i}\";" >> /etc/nginx/fastcgi_params_app
 done
-chown -R www-data:www-data /app
+chown -R www-data:www-data /app/storage
 supervisord -n
